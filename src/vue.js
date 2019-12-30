d01c2fdd0ed5772fb5982f823bf1026df2042e6d
Gsangu <gozhushi@qq.com>
2019-12-30 17:38:02 +0800

    feat: error report and v-stat

--- src/vue.js
+++ src/vue.js
@@ -20,10 +20,48 @@ const install = (Vue, options) => {
         throw new Error(`Like v-log="'click'"`)
       }
       el.addEventListener('click', function (event) {
-        Logger.send(value, event)
+        Logger.send(value, '', event)
       })
     }
   })
+  // v-stat
+  Vue.directive('stat', {
+    // 当被绑定的元素插入到 DOM 中时
+    inserted (el, binding, vNode) {
+      // 获取值
+      const { value } = binding
+      // 添加事件监听
+      if (!value) {
+        throw new Error(`Like v-stat="'view'"`)
+      }
+      Logger.send(value, '')
+    }
+  })
+  if (Logger.options.autoError) {
+    Vue.config.errorHandler = function (err, vm, info) {
+      const { message, stack } = err
+
+      // Processing error
+      let resourceUrl, col, line
+      let errs = stack.match(/\(.+?\)/)
+      if (errs && errs.length) errs = errs[0]
+      errs = errs.replace(/\w.+js/g, $1 => {
+        resourceUrl = $1; return ''
+      })
+      errs = errs.split(':')
+      if (errs && errs.length > 1) line = parseInt(errs[1] || 0); col = parseInt(errs[2] || 0)
+
+      const errorData = {
+        msg: message,
+        col: col,
+        line: line,
+        resourceUrl: resourceUrl
+      }
+        // 上报异常
+      Logger.send('error', errorData)
+      Logger.options.onError && Logger.options.onError(errorData)
+    }
+  }
   if (!Vue.prototype.$stat) {
     Object.defineProperty(Vue.prototype, `$stat`, { value: reporter.send })
   }
