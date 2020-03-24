33e366e0a6a8aa52f3e38cd01cdd1d81d4891663
Gsangu <gozhushi@qq.com>
2020-03-24 15:00:16 +0800

    feat: test file

---
+++ test/test.js
@@ -0,0 +1,8 @@
+/*
+ * FileName: test.js
+ * Project: wd-web-log
+ * Author: Gsan
+ * File Created: Tuesday, 24th March 2020 2:59:58 pm
+ * Last Modified: Tuesday, 24th March 2020 2:59:58 pm
+ * Modified By: Gsan
+ */
